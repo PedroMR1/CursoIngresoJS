853a4326088d622c5d253a7ef55a607fec2ad09f
PedroMR1 <60629002+PedroMR1@users.noreply.github.com>
2020-02-10 22:26:37 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,13 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+    var edad;
+    var nombre;
+    var mensaje;
+    
+    edad= parseInt(document.getElementById("laEdad").value);
+    nombre= document.getElementById("elNombre").value;
+    mensaje=("Usted se llama " +nombre + " y su edad es "+edad);
+    alert(mensaje);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,13 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var numeroUno;
+var numeroDos;
+var mensaje;
 
+numeroUno= parseInt(document.getElementById("numeroUno").value);
+numeroDos= parseInt(document.getElementById("numeroDos").value);
+mensaje= ("La suma es " + (numeroUno+numeroDos));
+alert (mensaje);
 }
 
