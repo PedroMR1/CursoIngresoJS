2be7eac5448e8b9ea29a298055f79b49bcb3c5c5
PedroMR1 <60629002+PedroMR1@users.noreply.github.com>
2020-02-24 15:03:46 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,20 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch (mesDelAño) {
+    case "Enero":
+        alert ("Que comiencen las clases!!!");
+        break;
+    case "Marzo":
+        alert ("a clases!!!!");    
+        break;
+    case "Julio":
+        alert ("se vienen las vacaciones!!!!");
+        break;
+    case "Diciembre":
+        alert ("Felices fiestas!!!!");
+        break;
+    }
 
 
 
